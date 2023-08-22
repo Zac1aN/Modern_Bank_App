acab41d6c6359f522582d4e6e24d1787bc0f645f
Suvanjan Paul <73705392+Zac1aN@users.noreply.github.com>
2023-08-22 22:43:00 +0530

    Add files via upload

---
+++ modern_bank_react/src/components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in a few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Discover an improved credit card offer effortlessly through a few simple steps, enhancing your financial choices with ease.Unlock superior card deals swiftly and efficiently.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
